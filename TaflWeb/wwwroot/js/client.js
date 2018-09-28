6bfd64179d3a19d4f623df4ed9962eb96419a08a
bivvend <simon.j.henley@gmail.com>
2018-09-28 20:33:10 +0100

    Drawing working

--- TaflWeb/wwwroot/js/client.js
+++ TaflWeb/wwwroot/js/client.js
@@ -6,14 +6,6 @@
     var blockSizeX = null;
     var blockSizeY = null;
 
-    var sqImage;
-
-    function loadImages() {
-        sqImage = new Image();
-        sqImage.src = "../Images/tile1.bmp";
-        sqImage.onload = draw();
-    }
-
     function getString() {
         $.ajax({
             url: "/api/Game/GetString", success: function (result) {
@@ -26,18 +18,27 @@
             url: "/api/Game/GetBoard", success: function (result) {
                 $("#jsonBoard").text(result);
                 boardData = JSON.parse(result);
-                loadImages();
+                draw();
             }
         }); 
     }
+
+    function boardClick(x, y) {
+        var i = 0;
+    }
     
     function draw() {
-        var canvas = $("#boardImage")[0];
+        
+        var canvas = $("#boardImage")[0];  //Need to get DOM object
+        //Click needs to be registered to jQuery object not DOM object
+        $("#boardImage").click(function (e) {
+            mouseX = e.pageX - $("#boardImage").offset().left;
+            mouseY = e.pageY - $("#boardImage").offset().top;
+            boardClick(mouseX, mouseY);
+        });
         // Canvas supported?
         if (canvas.getContext) {
-            ctx = canvas.getContext('2d');
-
-
+            var ctx = canvas.getContext('2d');
 
             if (NUMBER_OF_COLUMNS != 0 && NUMER_OF_ROWS != 0) {
                 blockSizeX = Math.round(canvas.width / NUMBER_OF_COLUMNS) - 1;
@@ -48,53 +49,58 @@
                 blockSizeY = 80;
             }
 
+            var numberOfElements = NUMBER_OF_COLUMNS * NUMER_OF_ROWS;
 
-            var pat = ctx.createPattern(sqImage, "repeat");
-
-            var i, j, value;
-            for (i = 0; i < NUMBER_OF_COLUMNS; i++){
-                for (j = 0; j < NUMER_OF_ROWS; j++){
-                    value = boardData.SquareTypeArray[i][j];
-                    switch (value){
+            var drawnList = [];
+                        
+            for (let i = 0; i < NUMBER_OF_COLUMNS; i++){
+                for (let j = 0; j < NUMER_OF_ROWS; j++){
+                    var value = boardData.SquareTypeArray[i][j];
+                    var src;
+                    switch (value) {
                         case 0:
+                            src = '../Images/tile1.bmp';
                             break;
                         case 1:
+                            src = '../Images/throne.bmp';
                             break;
                         case 2:
+                            src = '../Images/attacktile1.bmp';
                             break;
                         case 3:
+                            src = '../Images/throne.bmp';
                             break;
                         case 4:
+                            src = '../Images/deftile1.bmp';
                             break;
                         default:
-                            break;
+                            src = '../Images/tile1.bmp';
+                            break
+                    }                    
+
+                    function loadImage(srcIn, x, y) {
+
+                        var imageObj = new Image();
+                        imageObj.src = srcIn;
+                        imageObj.onload = function () {
+                            ctx.drawImage(imageObj, x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
+                            drawnList.add("1");
+                        };
+
                     }
 
-                    ctx.drawImage(sqImage, i * blockSizeX, j * blockSizeY, blockSizeX, blockSizeY);
-                    
-
+                    loadImage(src, i, j);
+                   
                 }
-            }
-
-            //// Calculdate the precise block size
-            //BLOCK_SIZE = canvas.height / NUMBER_OF_ROWS;
-
-            //// Draw the background
-            //drawBoard();
-
-            //defaultPositions();
-
-            //// Draw pieces
-            //pieces = new Image();
-            //pieces.src = 'pieces.png';
-            //pieces.onload = drawPieces;
-
-            //canvas.addEventListener('click', board_click, false);
+            }   
+            
         }
         else {
             alert("Canvas not supported!");
         }
     }
+
+     
     getString();
     getBoardData();
 });
