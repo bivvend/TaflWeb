5ad131f55ca95c9730f756dd521a272f3c01990c
bivvend <simon.j.henley@gmail.com>
2018-11-08 08:33:09 +0000

    Rules page added

---
+++ TaflWeb/Controllers/RulesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TaflWeb.Models;
+
+namespace TaflWeb.Controllers
+{
+    public class RulesController : Controller
+    {
+
+
+        public RulesController()
+        {
+
+        }
+
+        public IActionResult Rules()
+        {
+            return View();
+        }
+    }
+}
